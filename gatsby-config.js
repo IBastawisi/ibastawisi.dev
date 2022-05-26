9faaed3b5453db3c9a911358dda82dbe1a1ad817
Ibrahim El-bastawisi <ibastawisi@outlook.com>
2022-05-27 01:33:19 +0200

    Update personal info

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,12 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `IBastawisi`,
+    siteTitleAlt: `IBastawisi - Portfolio`,
+    siteHeadline: `IBastawisi - Portfolio`,
+    siteUrl: `https://ibastawisi.dev`,
+    siteDescription: `Developer portfolio of IBastawisi`,
     siteLanguage: `en`,
-    siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
   },
   plugins: [
